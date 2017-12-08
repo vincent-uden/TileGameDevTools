0ef8f52c94e2580bfb1a4b2cf55dbe979b1d1904
xorvralin2 <vincentudengmail.com>
2017-12-08 17:44:10 +0100

    Added functionality to save and load maps

--- tile_map_maker.py
+++ tile_map_maker.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+import os
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 LARGE_FONT = ("Verdana", 12)
 SMALL_FONT = ("Verdana", 9)
@@ -9,185 +11,235 @@ tiles = []
 mouse_pressed = False
 # TODO: Change to save different material types
 def set_dimensions(x, y):
-    """
-    Populates tiles with None to according to x and y.
-    :param x: Int
-    :param y: Int
-    :return: None
-    """
-    while tiles != []:
-        tiles.pop()
-    for y1 in range(y):
-        tiles.append([None for x1 in range(x)])
+	"""
+	Populates tiles with None to according to x and y.
+	:param x: Int
+	:param y: Int
+	:return: None
+	"""
+	while tiles != []:
+		tiles.pop()
+	for y1 in range(y):
+		tiles.append(["NONE" for x1 in range(x)])
 
-def export_input(box1, box2):
-    inp1 = box1.get()
-    inp2 = box2.get()
-    try:
-        x = int(inp1)
-        y = int(inp2)
-        set_dimensions(x, y)
-    except ValueError:
-        win = tk.Toplevel(padx=20, pady=20)
-        win.wm_title("Error")
-        l = tk.Label(win, text="Please enter two integers", font=LARGE_FONT)
-        l.pack(side=tk.TOP)
+def export_input(box1, box2, frame):
+	inp1 = box1.get()
+	inp2 = box2.get()
+	try:
+		x = int(inp1)
+		y = int(inp2)
+		set_dimensions(x, y)
+		app.frames[StartPage].render_tiles()
+		frame.destroy()
+	except ValueError:
+		pass
 
 
 def new_map_popup():
-    win = tk.Toplevel(padx=20, pady=20)
-    win.wm_title("New Map")
+	win = tk.Toplevel(padx=20, pady=20)
+	win.wm_title("New Map")
 
-    l = tk.Label(win, text="Choose dimensions", font=LARGE_FONT)
-    l.pack(side=tk.TOP)
+	l = tk.Label(win, text="Choose dimensions", font=LARGE_FONT)
+	l.pack(side=tk.TOP)
 
-    dim_frame = tk.Frame(win)
-    x_l = tk.Label(dim_frame, text="X:")
-    x_l.pack(side=tk.LEFT)
-    x_box = tk.Entry(dim_frame, exportselection=0)
-    x_box.pack(side=tk.LEFT)
+	dim_frame = tk.Frame(win)
+	x_l = tk.Label(dim_frame, text="X:")
+	x_l.pack(side=tk.LEFT)
+	x_box = tk.Entry(dim_frame, exportselection=0)
+	x_box.pack(side=tk.LEFT)
 
-    y_box = tk.Entry(dim_frame, exportselection=0)
-    y_box.pack(side=tk.RIGHT)
-    y_l = tk.Label(dim_frame, text="Y:")
-    y_l.pack(side=tk.RIGHT)
-    dim_frame.pack()
+	y_box = tk.Entry(dim_frame, exportselection=0)
+	y_box.pack(side=tk.RIGHT)
+	y_l = tk.Label(dim_frame, text="Y:")
+	y_l.pack(side=tk.RIGHT)
+	dim_frame.pack()
 
-    confirm = ttk.Button(win, text="Confirm",
-                         command=lambda: export_input(x_box, y_box))
-    confirm.pack(side=tk.BOTTOM, pady=20)
+	confirm = ttk.Button(win, text="Confirm",
+						 command=lambda: export_input(x_box, y_box, win))
+	confirm.pack(side=tk.BOTTOM, pady=20)
 
 def save_map(name, window):
-    output = open(f"{name}.txt", "w")
-    for row in tiles:
-        text_row = " ".join([str(x) for x in row])
-        output.write(text_row)
-        output.write("\n")
-    output.close()
-    window.destroy()
+	output = open(f"{name}.txt", "w")
+	for row in tiles:
+		text_row = " ".join([str(x) for x in row])
+		output.write(text_row)
+		output.write("\n")
+	output.close()
+	window.destroy()
 
 def save_map_popup():
-    win = tk.Toplevel(padx=20, pady=20)
-    win.wm_title("Save Map")
+	win = tk.Toplevel(padx=20, pady=20)
+	win.wm_title("Save Map")
 
-    l = tk.Label(win, text="Enter map name", font=LARGE_FONT)
-    l.pack(side=tk.TOP)
+	l = tk.Label(win, text="Enter map name", font=LARGE_FONT)
+	l.pack(side=tk.TOP)
 
-    name_box = tk.Entry(win, exportselection=0)
-    name_box.pack()
-    name_box.bind("<Return>", lambda event: save_map(name_box.get(), win))
+	name_box = tk.Entry(win, exportselection=0)
+	name_box.pack()
+	name_box.bind("<Return>", lambda event: save_map(name_box.get(), win))
 
-    save_button = ttk.Button(win, text="Confirm",
-                             command=lambda: save_map(name_box.get(), win))
-    save_button.pack()
+	save_button = ttk.Button(win, text="Confirm",
+							 command=lambda: save_map(name_box.get(), win))
+	save_button.pack()
+
+def open_map(frame):
+	current_path = os.path.dirname(os.path.abspath(__file__))
+	name = askopenfilename(initialdir=current_path, filetypes=(("Map files", "*.txt"), ("All files", "*.*")), title="Choose a file")
+	try:
+		input_map = open(name)
+	except:
+		return
+	data = input_map.read()
+	input_map.close()
+	data = data.split("\n")[:-1]
+	new_data = []
+	while tiles != []:
+		tiles.pop()
+	for row in data:
+		tiles.append(row.split(" "))
+	frame.render_tiles()
 
 # This class will be root frame
 class TileMapCreator(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self ,*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		tk.Tk.__init__(self ,*args, **kwargs)
 
-        tk.Tk.wm_title(self, "Tile Map Creator 1.0")
+		tk.Tk.wm_title(self, "Tile Map Creator 1.0")
 
-        container = tk.Frame(self, padx=0, pady=0)
-        container.pack(side="top", fill="both", expand=True)
+		container = tk.Frame(self, padx=0, pady=0)
+		container.pack(side="top", fill="both", expand=True)
 
-        self.frames = {}
+		self.frames = {}
 
-        for F in (StartPage, Page2):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+		for F in (StartPage, Page2):
+			frame = F(container, self)
+			self.frames[F] = frame
+			frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+		self.show_frame(StartPage)
 
-        menu_bar = tk.Menu(self)
-        # TODO: Add open(load) functionality
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="New Map", command=new_map_popup)
-        file_menu.add_command(label="Save", command=save_map_popup)
-        file_menu.add_command(label="Open")
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.quit)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        self.config(menu=menu_bar)
+		menu_bar = tk.Menu(self)
+		# TODO: Add open(load) functionality
+		file_menu = tk.Menu(menu_bar, tearoff=0)
+		file_menu.add_command(label="New Map", command=new_map_popup)
+		file_menu.add_command(label="Save", command=save_map_popup)
+		file_menu.add_command(label="Open", command=lambda:open_map(self.frames[StartPage]))
+		file_menu.add_separator()
+		file_menu.add_command(label="Exit", command=self.quit)
+		menu_bar.add_cascade(label="File", menu=file_menu)
+		self.config(menu=menu_bar)
 
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
+	def show_frame(self, cont):
+		frame = self.frames[cont]
+		frame.tkraise()
 
 class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
 
-        self.canvas = tk.Canvas(self, width=800, height=800, background="white")
-        self.canvas.grid(column=0, row=0)
-        set_dimensions(40, 40)
+		self.canvas = tk.Canvas(self, width=800, height=800, background="white")
+		self.canvas.grid(column=0, row=0)
+		set_dimensions(40, 40)
 
-        self.selected_tool = self.draw
-        self.selected_size = 1
+		self.selected_tool = self.draw
+		self.selected_size = 1
 
-        self.canvas.bind("<Button-1>", self.selected_tool)
-        self.canvas.bind("<B1-Motion>", self.selected_tool)
+		self.canvas.bind("<Button-1>", self.selected_tool)
+		self.canvas.bind("<B1-Motion>", self.selected_tool)
 
-        self.selected_colour = "Erase"
-        self.colors = {
-                       "Erase": ("#ffffff", "None"),
-                       "Red"  : ("#ff0000", "RED1"),
-                       "Green": ("#00ff00", "LGRE"),
-                       "Blue" : ("#2659f2", "WBLU"),
-                       "Brown": ("#9b6700", "BRWN")
-                       }
+		self.selected_colour = "Red"
+		self.colors = {
+					   "Red"  : ("#ff0000", "RED1"),
+					   "Green": ("#00ff00", "LGRE"),
+					   "Blue" : ("#2659f2", "WBLU"),
+					   "Brown": ("#9b6700", "BRWN"),
+					   "Black": ("#000000", "BLCK")
+					   }
+		self.internal_colors = {
+								"NONE":"#ffffff",
+								"RED1":"#ff0000",
+								"LGRE":"#00ff00",
+								"WBLU":"#2659f2",
+								"BRWN":"#9b6700",
+								"BLCK":"#000000"
+								}
 
-        default_color = tk.StringVar(self)
-        default_color.set("Erase")
+		default_color = tk.StringVar(self)
+		default_color.set("Red")
 
-        right_frame = tk.Frame(self)
-        right_frame.grid(column=1, row=0, sticky="N")
+		right_frame = tk.Frame(self)
+		right_frame.grid(column=1, row=0, sticky="N")
 
-        color_menu = tk.OptionMenu(right_frame, default_color, *list(self.colors.keys()), command=self.select_material)
-        color_menu.grid(column=0, row=0, sticky="EW")
+		color_menu = tk.OptionMenu(right_frame, default_color, *list(self.colors.keys()), command=self.select_material)
+		color_menu.grid(column=0, row=0, sticky="EW")
 
-        # Tools
-        brush_tool = ttk.Button(right_frame, text="Brush",
-                                command=lambda:self.select_tool(self.draw))
-        brush_tool.grid(row=1)
+		# Tools
+		size_label = tk.Label(right_frame, text="Brush size", font=SMALL_FONT)
+		size_label.grid(row=1)
 
-        default_size = tk.IntVar(self)
-        default_size.set(1)
-        self.brush_size = tk.Scale(right_frame, from_=1, to=15, orient=tk.HORIZONTAL, variable=default_size, command=self.select_size)
-        self.brush_size.grid(row=2)
+		default_size = tk.IntVar(self)
+		default_size.set(1)
+		self.brush_size = tk.Scale(right_frame, from_=1, to=15, orient=tk.HORIZONTAL, variable=default_size, command=self.select_size)
+		self.brush_size.grid(row=2)
 
-    def draw(self, event):
-        x = event.x
-        y = event.y
-        x = x // 20 * 20
-        y = y // 20 * 20
-        self.canvas.create_rectangle(x, y, x + self.selected_size * 20, y + self.selected_size * 20, fill=self.colors[self.selected_colour], width=0)
-        for ix in range(self.selected_size):
-            for iy in range(self.selected_size):
-                try:
-                    tiles[(y + iy * 20) // 20][(x + ix * 20) // 20] = self.colors[self.selected_colour][1]
-                except:
-                    pass
+		brush_tool = ttk.Button(right_frame, text="Draw",
+								command=lambda:self.select_tool(self.draw))
+		brush_tool.grid(row=3)
 
-    def select_material(self, mat):
-        self.selected_colour = mat
+		erase_tool = ttk.Button(right_frame, text="Erase",
+								command=lambda:self.select_tool(self.erase))
+		erase_tool.grid(row=4)
 
-    def select_tool(self, tool):
-        self.selected_tool = tool
-        self.canvas.bind("<Button-1>", self.selected_tool)
-        self.canvas.bind("<B1-Motion>", self.selected_tool)
+	def draw(self, event):
+		x = event.x
+		y = event.y
+		x = x // 20 * 20
+		y = y // 20 * 20
+		self.canvas.create_rectangle(x, y, x + self.selected_size * 20, y + self.selected_size * 20, fill=self.colors[self.selected_colour], width=0)
+		for ix in range(self.selected_size):
+			for iy in range(self.selected_size):
+				try:
+					tiles[(y + iy * 20) // 20][(x + ix * 20) // 20] = self.colors[self.selected_colour][1]
+				except:
+					pass
+	
+	def erase(self, event):
+		x = event.x
+		y = event.y
+		x = x // 20 * 20
+		y = y // 20 * 20
+		self.canvas.create_rectangle(x, y, x + self.selected_size * 20, y + self.selected_size * 20, fill="white", width=0)
+		for ix in range(self.selected_size):
+			for iy in range(self.selected_size):
+				try:
+					tiles[(y + iy * 20) // 20][(x + ix * 20) // 20] = "NONE"
+				except:
+					pass
 
-    def select_size(self, size):
-        self.selected_size = int(size)
+	def render_tiles(self):
+		y_dim = len(tiles)
+		y_dim = len(tiles[0])
+		for y, row in enumerate(tiles):
+			for x, item in enumerate(row):
+				self.canvas.create_rectangle(x * 20, y * 20, (x + self.selected_size) * 20, (y + self.selected_size) * 20, fill=self.internal_colors[item], width=0)
+
+	def select_material(self, mat):
+		self.selected_colour = mat
+
+	def select_tool(self, tool):
+		self.selected_tool = tool
+		self.canvas.bind("<Button-1>", self.selected_tool)
+		self.canvas.bind("<B1-Motion>", self.selected_tool)
+
+	def select_size(self, size):
+		self.selected_size = int(size)
 
 
 
 class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
 
 
 app = TileMapCreator()
 app.mainloop()
-
